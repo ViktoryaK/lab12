ee2ae24a2a715d5f3294943bdab467a14b46b26d
ViktoryaK <96166901+ViktoryaK@users.noreply.github.com>
2022-12-24 21:33:06 +0200

    u

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Document document = new SmartDocument("gs://oop-course/Geico-2021.png");
-//        document = TimedDocument(document);
-//        document = CashedDocument(document);
+        document = TimedDocument(document);
+        document = CashedDocument(document);
         System.out.println(document.parse());
 
 
