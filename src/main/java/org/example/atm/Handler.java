d736965cf5440aec9e7b89536ee18a317c7aed8a
ViktoryaK <96166901+ViktoryaK@users.noreply.github.com>
2023-01-03 15:50:57 +0200

    small changes

---
+++ src/main/java/org/example/atm/Handler.java
@@ -0,0 +1,10 @@
+package org.example.atm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class Handler {
+    @Setter @Getter
+    private Handler next;
+    public abstract void process(int price);
+}
---
+++ src/main/java/org/example/atm/Handler50.java
@@ -0,0 +1,12 @@
+package org.example.atm;
+
+public class Handler50 extends Handler{
+    @Override
+    public void process(int price) {
+        System.out.println(price / 50 + " * 50");
+        Handler next = getNext();
+        if (next!=null) {
+            getNext().process(price % 50);
+        }
+    }
+}
